3e2cb923edf010fd3e80395cd344e5d5b8a9c200
Lovely Singh <lovely1211zmn@gmail.com>
2025-02-20 15:10:41 +0530

    resolved merge comflicts

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // Define your base URL here once
-const API_URL = 'http://localhost:8000/api';
+
+// const API_URL = 'http://localhost:8000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
